65593e9fbe8551b889ddfc545d09b71bae05758b
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-10-27 18:23:40 +0100

    first commit

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  return new PrismaClient();
+};
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const db = globalThis.prisma ?? prismaClientSingleton()
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default db
+export default db;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db;
