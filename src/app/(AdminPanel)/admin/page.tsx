b67858b357c92e8bb7677a111328f7938042e515
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-01-09 08:12:14 +0100

    test

--- src/app/(AdminPanel)/admin/page.tsx
+++ src/app/(AdminPanel)/admin/page.tsx
@@ -11,7 +11,7 @@ const AdminPage = async () => {
 
   // Render dashboard cards
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 p-4 md:p-0 text-white">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 p-4 md:p-0">
       <DayshBoardCard  title="Sales" 
         subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} 
         body={formatCurrency(salesData.amount)} 
